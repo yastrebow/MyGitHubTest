baec5159a76ee4cff008f76854eb246bd98f6f26
yastrebow <yastrebow@gmail.com>
2021-05-25 09:37:06 +0300

    It is the first version for new branch

--- src/test/java/MyCalculatorTest.java
+++ src/test/java/MyCalculatorTest.java
@@ -15,24 +15,24 @@ public class MyCalculatorTest {
     @Test
     public void testAdd() {
         int result = 18;
-        Assert.assertEquals("11 + 7 must be equal 18", result, MyCalculator.addition(a, b));
+        Assert.assertEquals("11 + 7 must be equal 18", result, MyCalculator.add(a, b));
     }
 
     @Test
     public void testSub() {
         int result = 4;
-        Assert.assertEquals("11 - 7 must be equal 4", result, MyCalculator.subtraction(a, b));
+        Assert.assertEquals("11 - 7 must be equal 4", result, MyCalculator.sub(a, b));
     }
 
     @Test
     public void testMulti() {
         int result = 77;
-        Assert.assertEquals("11 * 7 must be equal 18", result, MyCalculator.multiplication(a, b));
+        Assert.assertEquals("11 * 7 must be equal 18", result, MyCalculator.multi(a, b));
     }
 
     @Test
     public void testDiv() {
         float result = 1.5714f;
-       Assert.assertEquals("11 / 7 must be equal about 1.5714", result, MyCalculator.divide(a, b), 0.0001);
+       Assert.assertEquals("11 / 7 must be equal about 1.5714", result, MyCalculator.div(a, b), 0.0001);
     }
 }
