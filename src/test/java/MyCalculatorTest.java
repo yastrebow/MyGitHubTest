debd23f55f4104c7c9c117e6c509200b4e25c731
yastrebow <yastrebow@gmail.com>
2021-05-25 09:13:55 +0300

    In this version I changed the value of the variables in the Test

--- src/test/java/MyCalculatorTest.java
+++ src/test/java/MyCalculatorTest.java
@@ -8,31 +8,31 @@ public class MyCalculatorTest {
 
     @Before
     public void setUp() {
-        a = 7;
-        b = 11;
+        a = 11;
+        b = 7;
     }
 
     @Test
     public void testAdd() {
         int result = 18;
-        Assert.assertEquals("7 + 11 must be equal 18", result, MyCalculator.addition(a, b));
+        Assert.assertEquals("11 + 7 must be equal 18", result, MyCalculator.addition(a, b));
     }
 
     @Test
     public void testSub() {
-        int result = -4;
-        Assert.assertEquals("7 - 11 must be equal -4", result, MyCalculator.subtraction(a, b));
+        int result = 4;
+        Assert.assertEquals("11 - 7 must be equal 4", result, MyCalculator.subtraction(a, b));
     }
 
     @Test
     public void testMulti() {
         int result = 77;
-        Assert.assertEquals("7 * 11 must be equal 18", result, MyCalculator.multiplication(a, b));
+        Assert.assertEquals("11 * 7 must be equal 18", result, MyCalculator.multiplication(a, b));
     }
 
     @Test
     public void testDiv() {
-        float result = 0.6363f;
-       Assert.assertEquals("7 / 11 must be equal about 0.6363", result, MyCalculator.divide(a, b), 0.0001);
+        float result = 1.5714f;
+       Assert.assertEquals("11 / 7 must be equal about 1.5714", result, MyCalculator.divide(a, b), 0.0001);
     }
 }
