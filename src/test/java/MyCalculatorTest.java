7d19bdad87fafa98aea817a20989332691ebf224
yastrebow <yastrebow@gmail.com>
2021-05-25 08:57:55 +0300

    In this version I changed method names. Also I changed the variable names in the Test

--- src/test/java/MyCalculatorTest.java
+++ src/test/java/MyCalculatorTest.java
@@ -3,36 +3,36 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MyCalculatorTest {
-    int x;
-    int y;
+    int a;
+    int b;
 
     @Before
     public void setUp() {
-        x = 7;
-        y = 11;
+        a = 7;
+        b = 11;
     }
 
     @Test
     public void testAdd() {
         int result = 18;
-        Assert.assertEquals("7 + 11 must be equal 18", result, MyCalculator.add(x, y));
+        Assert.assertEquals("7 + 11 must be equal 18", result, MyCalculator.addition(a, b));
     }
 
     @Test
     public void testSub() {
         int result = -4;
-        Assert.assertEquals("7 - 11 must be equal -4", result, MyCalculator.sub(x, y));
+        Assert.assertEquals("7 - 11 must be equal -4", result, MyCalculator.subtraction(a, b));
     }
 
     @Test
     public void testMulti() {
         int result = 77;
-        Assert.assertEquals("7 * 11 must be equal 18", result, MyCalculator.multi(x, y));
+        Assert.assertEquals("7 * 11 must be equal 18", result, MyCalculator.multiplication(a, b));
     }
 
     @Test
     public void testDiv() {
         float result = 0.6363f;
-       Assert.assertEquals("7 / 11 must be equal about 0.6363", result, MyCalculator.div(x, y), 0.0001);
+       Assert.assertEquals("7 / 11 must be equal about 0.6363", result, MyCalculator.divide(a, b), 0.0001);
     }
 }
