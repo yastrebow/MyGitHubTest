f1a7438e708296a28f6f67b4c7683b35ddf4d7ae
yastrebow <yastrebow@gmail.com>
2021-05-25 11:44:29 +0300

    It is the version of Test branch for creating conflict between two branches

--- src/test/java/MyCalculatorTest.java
+++ src/test/java/MyCalculatorTest.java
@@ -8,31 +8,31 @@ public class MyCalculatorTest {
 
     @Before
     public void setUp() {
-        m = 15;
+        m = 12;
         n = 3;
     }
 
     @Test
     public void testAdd() {
-        int result = 18;
-        Assert.assertEquals("15 + 3 must be equal 18", result, MyCalculator.add(m, n));
+        int result = 15;
+        Assert.assertEquals("12 + 3 must be equal 15", result, MyCalculator.add(m, n));
     }
 
     @Test
     public void testSub() {
-        int result = 12;
-        Assert.assertEquals("15 - 3 must be equal 12", result, MyCalculator.sub(m, n));
+        int result = 9;
+        Assert.assertEquals("12 - 3 must be equal 9", result, MyCalculator.sub(m, n));
     }
 
     @Test
     public void testMulti() {
-        int result = 45;
-        Assert.assertEquals("15 * 3 must be equal 45", result, MyCalculator.mult(m, n));
+        int result = 36;
+        Assert.assertEquals("12 * 3 must be equal 36", result, MyCalculator.mult(m, n));
     }
 
     @Test
     public void testDiv() {
-        float result = 5.0000f;
-       Assert.assertEquals("15 / 3 must be equal about 5", result, MyCalculator.div(m, n), 0.0001);
+        float result = 4.0000f;
+       Assert.assertEquals("12 / 3 must be equal about 4", result, MyCalculator.div(m, n), 0.0001);
     }
 }
